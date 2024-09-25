acdbe74ee2e8b7a3c68a5bddff2347e1bea74851
Samuel Johnny <johnnysamuel486@gmail.com>
2024-09-24 23:32:20 -0300

    alteração na parte de abrir o popup, agora  o body não pode ser scrolado e responsividade no container-o-que precisar melhorada

--- Scripts/script.js
+++ Scripts/script.js
@@ -2,5 +2,10 @@ const asideEl = document.querySelector(".aside");
 const menuEl = document.querySelector(".cabecalho__icone-menu");
 const closeMenuEl = document.querySelector(".aside__icone-menu");
 
-menuEl.addEventListener("click", () => (asideEl.style.display = "flex"));
+function openMenu() {
+  asideEl.style.display = "flex";
+  document.documentElement.style.overflow = "hidden";
+}
+
+menuEl.addEventListener("click", openMenu);
 closeMenuEl.addEventListener("click", () => (asideEl.style.display = "none"));
